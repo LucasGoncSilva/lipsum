b03b6830172f73aabb482f1b1baa0562a6c8ec93
Lucas Gonçalves da Silva <lucasgoncsilva04@gmail.com>
2022-06-07 15:31:52 -0300

    general improvements

--- secret/models.py
+++ secret/models.py
@@ -7,16 +7,32 @@ from accounts.models import User
 
 # Create your models here.
 class LoginCredential(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='credentials', verbose_name='Dono')
-    service = models.CharField(max_length=64, verbose_name='Serviço')
-    name = models.CharField(max_length=40, verbose_name='Apelido (ex: Conta Principal, Conta de Teste, Compartilhada)')
-    thirdy_party_login = models.BooleanField(verbose_name='Login com serviço de terceiro?')
-    thirdy_party_login_name = models.CharField(max_length=40, verbose_name='Apelido do serviço de terceiro')
-    login = models.CharField(max_length=200, verbose_name='Login')
-    password = models.CharField(max_length=200, verbose_name='Senha')
-    note = models.TextField(max_length=128, blank=True, null=True, verbose_name='Anotação particular')
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    owner: object = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='credentials',
+        verbose_name='Dono'
+    )
+    service: object = models.CharField(max_length=64, verbose_name='Serviço')
+    name: object = models.CharField(
+        max_length=40,
+        verbose_name='Apelido (ex: Conta Principal, Conta de Teste, Compartilhada)'
+    )
+    thirdy_party_login: object = models.BooleanField(verbose_name='Login com serviço de terceiro?')
+    thirdy_party_login_name: object = models.CharField(
+        max_length=40,
+        verbose_name='Apelido do serviço de terceiro'
+    )
+    login: object = models.CharField(max_length=200, verbose_name='Login')
+    password: object = models.CharField(max_length=200, verbose_name='Senha')
+    note: object = models.TextField(
+        max_length=128,
+        blank=True,
+        null=True,
+        verbose_name='Anotação particular'
+    )
+    created: object = models.DateTimeField(auto_now_add=True)
+    updated: object = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
         return f'{str(self.owner.first_name)} | {self.service} | {self.name}'
@@ -36,18 +52,38 @@ class Card(models.Model):
         ('Co-branded', 'Co-branded'),
     ]
 
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cards', verbose_name='Dono')
-    name = models.CharField(max_length=40, verbose_name='Apelido (ex: Cartão da Família, Cartão de Milhas)')
-    model = models.CharField(max_length=16, choices=cards_choices, verbose_name='Tipo (débito, crédito, ...)')
-    number = models.CharField(max_length=19, verbose_name='Número do Cartão')
-    expiration = MonthField(verbose_name='Data de Expiração')
-    cvv = models.CharField(max_length=4, verbose_name='cvv')
-    bank = models.CharField(max_length=64, verbose_name='Banco')
-    brand = models.CharField(max_length=64, verbose_name='Bandeira')
-    owners_name = models.CharField(max_length=64, verbose_name='Nome do Contratante (como no cartão)')
-    note = models.TextField(max_length=128, blank=True, null=True, verbose_name='Anotação Particular')
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    owner: object = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='cards',
+        verbose_name='Dono'
+    )
+    name: object = models.CharField(
+        max_length=40,
+        verbose_name='Apelido (ex: Cartão da Família, Cartão de Milhas)'
+    )
+    model: object = models.CharField(
+        max_length=16,
+        choices=cards_choices,
+        verbose_name='Tipo (débito, crédito, ...)'
+    )
+    number: object = models.CharField(max_length=19, verbose_name='Número do Cartão')
+    expiration: object = MonthField(verbose_name='Data de Expiração')
+    cvv: object = models.CharField(max_length=4, verbose_name='cvv')
+    bank: object = models.CharField(max_length=64, verbose_name='Banco')
+    brand: object = models.CharField(max_length=64, verbose_name='Bandeira')
+    owners_name: object = models.CharField(
+        max_length=64,
+        verbose_name='Nome do Contratante (como no cartão)'
+    )
+    note: object = models.TextField(
+        max_length=128,
+        blank=True,
+        null=True,
+        verbose_name='Anotação Particular'
+    )
+    created: object = models.DateTimeField(auto_now_add=True)
+    updated: object = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
         return f'{str(self.owner.first_name)} | {self.model} | {self.name}'
@@ -60,14 +96,19 @@ class Card(models.Model):
 
 
 class SecurityNote(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
-    name = models.CharField(max_length=40)
-    content = models.TextField(max_length=600)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    owner: object = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='notes',
+        verbose_name='Dono'
+    )
+    title: object = models.CharField(max_length=40, verbose_name='Título')
+    content: object = models.TextField(max_length=600, verbose_name='Conteúdo')
+    created: object = models.DateTimeField(auto_now_add=True)
+    updated: object = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
-        return f'{str(self.owner.first_name)} | {self.name}'
+        return f'{str(self.owner.first_name)} | {self.title}'
 
     def get_absolute_url(self) -> object:
         return reverse('secret:note_detail_view', args=(str(self.id)))
