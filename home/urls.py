2971b4150fa56158af157f47dc6d691df0c7d3bc
Lucas Gonçalves da Silva <lucasgoncsilva04@gmail.com>
2022-07-04 14:45:45 -0300

    home app refactored

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from django.urls.resolvers import URLPattern
 
 from . import views
 
 
 app_name = 'home'
 
-urlpatterns = [
+urlpatterns: list[URLPattern] = [
     path('', views.index, name='index'),
 ]
